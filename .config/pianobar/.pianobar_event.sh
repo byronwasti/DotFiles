906e16e0d0867361df64fce8e42504838d077dca
Byron Wasti <byron.wasti@gmail.com>
2015-01-07 23:16:17 -0500

    Deleted a bunch of un needed code

--- .config/pianobar/.pianobar_event.sh
+++ .config/pianobar/.pianobar_event.sh
@@ -9,61 +9,8 @@ done < <(grep -e '^\(title\|artist\|album\|stationName\|songStationName\|pRet\|p
 
 case "$1" in
    songstart)
-#       echo 'naughty.notify({title = "pianobar", text = "Now playing: ' "$title" ' by ' "$artist" '"})' | awesome-client -
-
        echo "Z$title -- $artist" > $PANEL_FIFO
-
-#       if [ "$rating" -eq 1 ]
-#       then
-#           kdialog --title pianobar --passivepopup "'$title' by '$artist' on '$album' - LOVED" 10
-#       else
-#           kdialog --title pianobar --passivepopup "'$title' by '$artist' on '$album'" 10
-#       fi
-#       # or whatever you like...
        ;;
-
-#   songfinish)
-#       # scrobble if 75% of song have been played, but only if the song hasn't
-#       # been banned
-#       if [ -n "$songDuration" ] && [ "$songDuration" -ne 0 ] &&
-#               [ $(echo "scale=4; ($songPlayed/$songDuration*100)>50" | bc) -eq 1 ] &&
-#               [ "$rating" -ne 2 ]; then
-#           # scrobbler-helper is part of the Audio::Scrobble package at cpan
-#           # "pia" is the last.fm client identifier of "pianobar", don't use
-#           # it for anything else, please
-#           scrobbler-helper -P pia -V 1.0 "$title" "$artist" "$album" "" "" "" "$((songDuration/1000))" &
-#       fi
-#       ;;
-
-#   songlove)
-#       kdialog --title pianobar --passivepopup "LOVING '$title' by '$artist' on '$album' on station '$stationName'" 10
-#       ;;
-
-#   songshelf)
-#       kdialog --title pianobar --passivepopup "SHELVING '$title' by '$artist' on '$album' on station '$stationName'" 10
-#       ;;
-
-#   songban)
-#       kdialog --title pianobar --passivepopup "BANNING '$title' by '$artist' on '$album' on station '$stationName'" 10
-#       ;;
-
-#   songbookmark)
-#       kdialog --title pianobar --passivepopup "BOOKMARKING '$title' by '$artist' on '$album'" 10
-#       ;;
-
-#   artistbookmark)
-#       kdialog --title pianobar --passivepopup "BOOKMARKING '$artist'" 10
-#       ;;
-
-    *)
-        if [ "$pRet" -ne 1 ]; then
-            echo "naughty.notify({title = \"pianobar\", text = \"$1 failed: $pRetStr\"})" | awesome-client -
-#           kdialog --title pianobar --passivepopup "$1 failed: $pRetStr"
-        elif [ "$wRet" -ne 1 ]; then
-            echo "naughty.notify({title = \"pianobar\", text = \"$1 failed: Network error: $wRetStr\"})" | awesome-client -
-#           kdialog --title pianobar --passivepopup "$1 failed: Network error: $wRetStr"
-        fi
-        ;;
 esac
 
 
